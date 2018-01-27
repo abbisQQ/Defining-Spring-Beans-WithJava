452224d548b7eb4aedfc0dc4f35584bfed19de28
Ch. Theodorou <theodoroucharalabos@gmail.com>
2018-01-27 17:51:07 +0200

    Create SadFortuneService.java

---
+++ SadFortuneService.java
@@ -0,0 +1,14 @@
+package com.spring.tutorials;
+
+public class SadFortuneService implements FortuneService{
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return "Today is a bad day";
+	}
+
+	
+	
+	
+}
